855f69be86ea900d5ad8d88d8cf0c62790179f41
mirea70 <deae670@daum.net>
2023-01-16 16:53:26 +0900

    greedy 4번 solved

---
+++ src/main/java/com/exercise/ps/Programmers/greedy/greedy_4.java
@@ -0,0 +1,35 @@
+package com.exercise.ps.Programmers.greedy;
+
+import java.util.*;
+
+public class greedy_4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class greedy_4_Solution {
+    public int solution(int[] people, int limit) {
+        int boat = 0;
+        // people을 리스트에 넣는다
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : people) list.add(i);
+        // 리스트를 오름차순 정렬
+        Collections.sort(list);
+        // 다시 덱에 넣는다
+        ArrayDeque<Integer> dq = new ArrayDeque<>(50005);
+        for(int l : list) dq.add(l);
+        // 덱이 빌 때 까지 반복
+        while(!dq.isEmpty()){
+            // 맨뒤를 빼고 짝이 있는지 확인(맨앞)
+            // 맨앞과 더했을 떄 limit 보다 작다면 맨앞도 지워주기
+            // boat++
+            int last = dq.pollLast();
+            if(!dq.isEmpty() && last + dq.peekFirst() <= limit) dq.pollFirst();
+
+            boat++;
+            // 바깥 반복문 종료
+        }
+        return boat;
+    }
+}
