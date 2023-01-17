f931e8117b49cf41458ec1c82455f54c5454c021
GrowMyself <deae670@daum.net>
2023-01-17 22:45:22 +0900

    greedy_5 try

---
+++ src/main/java/com/exercise/ps/Programmers/greedy/greedy_5.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.Programmers.greedy;
+
+public class greedy_5 {
+}
+class greedy_5_Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        // costs 비용 기준 오름차순 정렬
+        // costs 순회
+        // 비용을 누적해나간다
+        // 건설 횟수가 >= n - 1이면 모든 섬이 연결되었는지 확인
+        // 모든 섬이 연결된 상태면 반복문 탈출
+        return answer;
+    }
+}
