1af2629511ac7b5559ad71f0b573ea67c58bb6dc
GrowMyself <deae670@daum.net>
2023-09-13 22:44:51 +0900

    str: 1427 풀이

---
+++ src/main/java/com/exercise/ps/Baekjoon/str/Q1427.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.Baekjoon.str;
+
+import java.util.*;
+import java.io.*;
+
+public class Q1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+        // 우선순위 큐 출력
+        for(int i=arr.length-1; i>=0; i--) System.out.print(arr[i]);
+    }
+}
