000893b41c4cc7b4051114ea83981d33953e40c1
GrowMyself <deae670@daum.net>
2024-04-19 21:47:37 +0900

    array: Rotate Array Solve

---
+++ src/main/java/com/exercise/ps/leetcode/RotateArray.java
@@ -0,0 +1,22 @@
+package com.exercise.ps.leetcode;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        reverse(nums, 0, len-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, len-1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+
+            start++;
+            end--;
+        }
+    }
+}
