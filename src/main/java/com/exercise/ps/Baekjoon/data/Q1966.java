f5b3209a89958266505e4c70868b5dfe31effc54
GrowMyself <deae670@daum.net>
2024-04-25 16:43:54 +0900

    data: 1966 Solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/data/Q1966.java
@@ -0,0 +1,42 @@
+package com.exercise.ps.Baekjoon.data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1966 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            String[] arr = br.readLine().split(" ");
+            int n = Integer.parseInt(arr[0]);
+            int m = Integer.parseInt(arr[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Deque<int[]> queue = new ArrayDeque<>();
+            for(int j=0; j<n; j++) {
+                queue.add(new int[]{j+1, Integer.parseInt(st.nextToken())});
+            }
+            int res = -1;
+            boolean isExtract;
+            int cnt = 1;
+            while(!queue.isEmpty()) {
+                isExtract = true;
+                int[] now = queue.poll();
+                for(int[] doc : queue) {
+                    if(doc[1] > now[1]) {
+                        queue.add(now);
+                        isExtract = false;
+                        break;
+                    }
+                }
+                if(isExtract) {
+                    if(now[0] == m+1) res = cnt;
+                    cnt++;
+                }
+            }
+            System.out.println(res);
+        }
+    }
+}
