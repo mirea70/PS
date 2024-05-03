abd221c0cf30b9595b91a7ac90710951c80bc981
GrowMyself <deae670@daum.net>
2024-05-03 21:01:04 +0900

    dp: leetcode 198 Solve

---
+++ src/main/java/com/exercise/ps/leetcode/dp/HouseRobber.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.leetcode.dp;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int rub = 0;
+        int notRub = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            int curRub = notRub + nums[i];
+            notRub = Math.max(notRub, rub);
+            rub = curRub;
+        }
+        return Math.max(rub, notRub);
+    }
+}
