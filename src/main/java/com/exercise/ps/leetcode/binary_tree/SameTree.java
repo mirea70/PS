fe90632f8c824a64fe91f4f8c9e2637b49098cb4
GrowMyself <deae670@daum.net>
2024-05-08 23:29:39 +0900

    binary_tree: leetcode 100 Solve

---
+++ src/main/java/com/exercise/ps/leetcode/binary_tree/SameTree.java
@@ -0,0 +1,36 @@
+package com.exercise.ps.leetcode.binary_tree;
+
+public class SameTree {
+
+    public boolean isSameTree(CustomTreeNode p, CustomTreeNode q) {
+        if(p == null) {
+            if(q == null) return true;
+            else return false;
+        }
+        else {
+            if(q == null) return false;
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+
+    public static class CustomTreeNode {
+        int val;
+        CustomTreeNode left;
+        CustomTreeNode right;
+
+        CustomTreeNode() {
+        }
+
+        CustomTreeNode(int val) {
+            this.val = val;
+        }
+
+        CustomTreeNode(int val, CustomTreeNode left, CustomTreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
