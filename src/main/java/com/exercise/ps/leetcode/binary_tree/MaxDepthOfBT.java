b45b8f59a59d36d4e2bb88d91fb37b9c42908850
GrowMyself <deae670@daum.net>
2024-05-08 23:27:27 +0900

    binary_tree: leetcode 104 Solve

---
+++ src/main/java/com/exercise/ps/leetcode/binary_tree/MaxDepthOfBT.java
@@ -0,0 +1,40 @@
+package com.exercise.ps.leetcode.binary_tree;
+
+import javax.swing.tree.TreeNode;
+
+public class MaxDepthOfBT {
+    private int maxDepth = 0;
+
+    public int maxDepth(CustomTreeNode root) {
+        if (root == null) return 0;
+        findDepth(root, 1);
+        return maxDepth;
+    }
+
+    private void findDepth(CustomTreeNode current, int depth) {
+        maxDepth = Math.max(maxDepth, depth);
+        if (current.left != null)
+            findDepth(current.left, depth + 1);
+        if (current.right != null)
+            findDepth(current.right, depth + 1);
+    }
+
+    public static class CustomTreeNode {
+        int val;
+        CustomTreeNode left;
+        CustomTreeNode right;
+
+        CustomTreeNode() {
+        }
+
+        CustomTreeNode(int val) {
+            this.val = val;
+        }
+
+        CustomTreeNode(int val, CustomTreeNode left, CustomTreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
