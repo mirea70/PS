e47f63ab0b38a3d4d3743c2bd61fbdfaccf4582e
GrowMyself <deae670@daum.net>
2024-05-29 23:13:42 +0900

    array: leetcode 122 Solve

---
+++ src/main/java/com/exercise/ps/leetcode/array/BestTimeStock2.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.leetcode.array;
+
+public class BestTimeStock2 {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 1) return 0;
+        int totalProfit = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i] > prices[i-1]) {
+                totalProfit += prices[i] - prices[i-1];
+            }
+        }
+        return totalProfit;
+    }
+}
