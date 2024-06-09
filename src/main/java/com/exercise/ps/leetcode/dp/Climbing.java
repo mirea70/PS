56c1286ea67166e10273a27d0986507115a06332
GrowMyself <deae670@daum.net>
2024-06-09 21:54:10 +0900

    dp: 70. Climbing Stairs

---
+++ src/main/java/com/exercise/ps/leetcode/dp/Climbing.java
@@ -0,0 +1,20 @@
+package com.exercise.ps.leetcode.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Climbing {
+    Map<Integer,Integer> map = new HashMap<>();
+
+    public int climbStairs(int n) {
+        return rec(n);
+    }
+
+    private int rec(int k) {
+        if(k == 1) return 1;
+        if(k == 2) return 2;
+        if(map.containsKey(k)) return map.get(k);
+        map.put(k, rec(k-1) + rec(k-2));
+        return map.get(k);
+    }
+}
