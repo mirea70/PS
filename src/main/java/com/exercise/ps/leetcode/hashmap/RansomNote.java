6da4445f53e95691b9c12ba9d66c9944fcfe630d
GrowMyself <deae670@daum.net>
2024-06-10 22:56:08 +0900

    data: 383. Ransom Note

---
+++ src/main/java/com/exercise/ps/leetcode/hashmap/RansomNote.java
@@ -0,0 +1,21 @@
+package com.exercise.ps.leetcode.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : magazine.toCharArray()) {
+            int value = map.getOrDefault(ch, 0);
+            map.put(ch, value + 1);
+        }
+
+        for(char c : ransomNote.toCharArray()) {
+            int value = map.getOrDefault(c, 0);
+            if(value == 0) return false;
+            map.put(c, value - 1);
+        }
+        return true;
+    }
+}
