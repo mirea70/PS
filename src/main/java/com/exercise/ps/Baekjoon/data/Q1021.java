00a952b22b2884cca529d420df0be6a7adbf5ff7
GrowMyself <deae670@daum.net>
2024-06-11 23:34:47 +0900

    data: 1021 solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/data/Q1021.java
@@ -0,0 +1,41 @@
+package com.exercise.ps.Baekjoon.data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import  java.util.*;
+
+public class Q1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[0]);
+        String[] positions = br.readLine().split(" ");
+
+        LinkedList<Integer> dq = new LinkedList<>();
+        for(int i=1; i<=n; i++)
+            dq.add(i);
+        int ans = 0;
+        for(String position : positions) {
+            int target = Integer.parseInt(position);
+            int current = dq.peekFirst();
+            if(current != target) {
+                if(dq.lastIndexOf(target) <= dq.size()/2) {
+                    while(dq.peekFirst() != target) {
+                        dq.addLast(dq.pollFirst());
+                        ans++;
+                    }
+                }
+                else {
+                    while(dq.peekFirst() != target) {
+                        dq.addFirst(dq.pollLast());
+                        ans++;
+                    }
+                }
+            }
+            dq.pollFirst();
+        }
+        System.out.print(ans);
+    }
+}
