5e245981afbbd93d3b963f5bc82a6ea72ad11332
GrowMyself <deae670@daum.net>
2024-07-12 23:36:37 +0900

    백준 Q1018 solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/bruteforce/Q1018.java
@@ -0,0 +1,10 @@
+package com.exercise.ps.Baekjoon.bruteforce;
+
+import java.util.*;
+
+public class Q1018 {
+    public static void main(String[] args) {
+        // 8,8 정사각형 자르기
+        // 탐색 -> 다시칠해야하는지 확인
+    }
+}
