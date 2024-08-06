9052c4f563b99bd802d1dec367b445e47b97f3ff
GrowMyself <deae670@daum.net>
2024-08-06 23:49:47 +0900

    baekjoon: 1655 풀이

---
+++ src/main/java/com/exercise/ps/Baekjoon/data/Q1655.java
@@ -0,0 +1,23 @@
+package com.exercise.ps.Baekjoon.data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1655 {
+    public static void main(String[] args) throws IOException {
+        // 1
+        // 1 / 5
+        // -99 1 5 / 5 7 10
+        TreeSet<Integer> set = new TreeSet<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int mid = -10000;
+        for(int i=0; i<n; i++) {
+
+
+        }
+    }
+}
