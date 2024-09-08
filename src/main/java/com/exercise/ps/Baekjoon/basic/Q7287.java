abb1e2ed52a1c0b48438b90ab95243de019716fa
GrowMyself <deae670@daum.net>
2024-09-08 12:13:10 +0900

    basic: 7287 solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/basic/Q7287.java
@@ -0,0 +1,11 @@
+package com.exercise.ps.Baekjoon.basic;
+
+import java.io.IOException;
+
+public class Q7287 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(224);
+        System.out.println("mirea70");
+    }
+}
