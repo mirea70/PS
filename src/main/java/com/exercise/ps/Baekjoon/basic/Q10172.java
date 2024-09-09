00a505670a72ee563e65b96b0b22db6e74be2d20
GrowMyself <deae670@daum.net>
2024-09-09 14:14:33 +0900

    basic: 10172 solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/basic/Q10172.java
@@ -0,0 +1,14 @@
+package com.exercise.ps.Baekjoon.basic;
+
+import java.io.IOException;
+
+public class Q10172 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
