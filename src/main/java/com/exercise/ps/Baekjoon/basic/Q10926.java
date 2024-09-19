ee1df1d11de564aa458a040bbe5b9a0bf10ec189
GrowMyself <deae670@daum.net>
2024-09-19 14:15:32 +0900

    basic: 10926 solve

---
+++ src/main/java/com/exercise/ps/Baekjoon/basic/Q10926.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.Baekjoon.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String alreadyId = br.readLine();
+        System.out.print(alreadyId + "??!");
+    }
+}
