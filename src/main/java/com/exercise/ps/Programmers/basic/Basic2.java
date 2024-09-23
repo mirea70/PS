e7592d2e8aa07f8925a10b53755f8b761ad0cdbe
GrowMyself <deae670@daum.net>
2024-09-24 08:11:33 +0900

    basic: programmers 2 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic2.java
@@ -0,0 +1,9 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic2 {
+    public String solution(int n) {
+        String answer = "";
+        answer = String.valueOf(n);
+        return answer;
+    }
+}
