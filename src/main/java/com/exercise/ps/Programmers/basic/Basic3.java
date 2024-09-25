e6a470ed1134ac302857a5970bbbf89137ed4e16
GrowMyself <deae670@daum.net>
2024-09-25 09:13:52 +0900

    basic: programmers 3 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic3.java
@@ -0,0 +1,9 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic3 {
+    public int solution(String n_str) {
+        int answer = 0;
+        answer = Integer.parseInt(n_str);
+        return answer;
+    }
+}
