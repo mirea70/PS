9d8fbfa6a08cbc53d51594cccf4c96e5bd2fae3f
GrowMyself <deae670@daum.net>
2024-10-19 10:02:33 +0900

    basic: programmers 28 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic28.java
@@ -0,0 +1,19 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic28 {
+    public int solution(int[] num_list) {
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for(int i=0; i<num_list.length; i++) {
+            if(i % 2 == 0) {
+                sum1 += num_list[i];
+            }
+            else
+                sum2 += num_list[i];
+        }
+
+        return Math.max(sum1,sum2);
+    }
+}
