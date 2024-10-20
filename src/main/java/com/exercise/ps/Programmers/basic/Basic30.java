5152265345d6e008d3f9857b43e099807d1a67e0
GrowMyself <deae670@daum.net>
2024-10-21 08:03:55 +0900

    basic: programmers 30 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic30.java
@@ -0,0 +1,10 @@
+package com.exercise.ps.Programmers.basic;
+
+import java.util.Arrays;
+
+public class Basic30 {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = Arrays.copyOfRange(num_list, 0, n);
+        return answer;
+    }
+}
