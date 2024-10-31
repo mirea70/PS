2e95877405dbd25fe04639820d3934856a7dac82
GrowMyself <deae670@daum.net>
2024-10-31 14:52:30 +0900

    basic: programmers 40 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic40.java
@@ -0,0 +1,7 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic40 {
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length()-n);
+    }
+}
