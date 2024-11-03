afacd286fd19dfe2d982e44b1b756d7ec82f594c
GrowMyself <deae670@daum.net>
2024-11-03 09:03:01 +0900

    basic: programmers 43 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic43.java
@@ -0,0 +1,18 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic43 {
+    public int solution(int n) {
+        int answer = 0;
+
+        if(n % 2 != 0) {
+            for(int i=1; i<=n; i= i+2)
+                answer += i;
+        }
+        else {
+            for(int i=2; i<=n; i= i+2)
+                answer += i*i;
+        }
+
+        return answer;
+    }
+}
