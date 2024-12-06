8bab1b76c22460086deaceec55d78e0581a84596
GrowMyself <deae670@daum.net>
2024-12-07 01:23:26 +0900

    basic: Programmers Basic54 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic54.java
@@ -0,0 +1,10 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic54 {
+    public int solution(int num, int n) {
+        if(num % n == 0)
+            return 1;
+        else
+            return 0;
+    }
+}
