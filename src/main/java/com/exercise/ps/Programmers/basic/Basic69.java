d4f529a7e1b131bcb21b9285659843907bf3fd09
GrowMyself <deae670@daum.net>
2024-12-22 12:33:07 +0900

    basic: Programmers Basic69 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic69.java
@@ -0,0 +1,15 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic69 {
+    public int[] solution(int[] arr, int k) {
+        for(int i=0; i<arr.length; i++) {
+            if(k % 2 != 0) {
+                arr[i] *= k;
+            }
+            else
+                arr[i] += k;
+        }
+
+        return arr;
+    }
+}
