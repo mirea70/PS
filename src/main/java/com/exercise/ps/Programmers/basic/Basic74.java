d160b913252b494f29227ce2a750b2301f9ad72c
GrowMyself <deae670@daum.net>
2024-12-27 13:26:06 +0900

    basic: Programmers Basic74 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic74.java
@@ -0,0 +1,12 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic74 {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        for(String str : str_list) {
+            if(!str.contains(ex))
+                sb.append(str);
+        }
+        return sb.toString();
+    }
+}
