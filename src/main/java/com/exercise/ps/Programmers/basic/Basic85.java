55c1b08869981caf032c8361100cb842ecf730aa
GrowMyself <deae670@daum.net>
2025-01-07 17:06:19 +0900

    basic: Programmers Basic85 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic85.java
@@ -0,0 +1,16 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic85 {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+
+        int len = str1.length();
+
+        for(int i=0; i<len; i++) {
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
