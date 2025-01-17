d29e6ed6dee9c2261b34686709ae2b27c0443e97
GrowMyself <deae670@daum.net>
2025-01-18 08:10:25 +0900

    basic: Programmers Basic96 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic96.java
@@ -0,0 +1,7 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic96 {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        return (x1 || x2) && (x3 || x4);
+    }
+}
