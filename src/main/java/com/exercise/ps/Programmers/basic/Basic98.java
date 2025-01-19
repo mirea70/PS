af9ea150e48502e574a9592adb70274f544adb38
GrowMyself <deae670@daum.net>
2025-01-20 02:11:27 +0900

    basic: Programmers Basic98 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic98.java
@@ -0,0 +1,18 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic98 {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i=0; i<queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            for(int j=s; j<=e; j++) {
+                if(j % k ==0)
+                    arr[j]++;
+            }
+        }
+
+        return arr;
+    }
+}
