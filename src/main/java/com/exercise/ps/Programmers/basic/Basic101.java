739b9af06062a894ad137e6b4dd207f03a132d00
mirea70 <deae670@daum.net>
2025-05-04 08:32:44 +0900

    basic: 101 solve

---
+++ src/main/java/com/exercise/ps/Programmers/basic/Basic101.java
@@ -0,0 +1,14 @@
+package com.exercise.ps.Programmers.basic;
+
+public class Basic101 {
+    public String solution(int q, int r, String code) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<code.length(); i++) {
+            if(i % q == r)
+                sb.append(code.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
