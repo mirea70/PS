d5ef061461e58d62a30fe488aae9ab89845b6790
GrowMyself <deae670@daum.net>
2025-07-05 23:54:58 +0900

    dp: 12920 solve

--- src/main/java/com/exercise/ps/Baekjoon/dp/Q12920.java
+++ src/main/java/com/exercise/ps/Baekjoon/dp/Q12920.java
@@ -13,37 +13,29 @@ public class Q12920 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N+1][M+1];
-        int[] v = new int[N+1];
-        int[] c = new int[N+1];
-        int[] k = new int[N+1];
+        int[] dp = new int[M+1];
 
         for(int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
-            v[i] = Integer.parseInt(st.nextToken());
-            c[i] = Integer.parseInt(st.nextToken());
-            k[i] = Integer.parseInt(st.nextToken());
-        }
+            int v = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=M; j++) {
-                // 우선 이전값 그대로 가져오기
-                dp[i][j] = dp[i-1][j];
+            int cnt = 1;
+            while(k > 0) {
+                int use = Math.min(cnt, k);
+                int weight = v * use;
+                int value = c * use;
 
-                for(int l=1; l<=k[i]; l++) {
-                    updateDp(dp, v[i], c[i], j, l, i);
+                for (int j=M; j>=weight; j--) {
+                    dp[j] = Math.max(dp[j], dp[j - weight] + value);
                 }
+
+                k -= use;
+                cnt <<= 1;
             }
         }
-        System.out.print(dp[N][M]);
-    }
 
-    // 현재 물건을 k만큼 넣을 수 있을 때 dp 갱신
-    private static void updateDp(int[][] dp, int cv, int cc, int weight, int k, int i) {
-        if(k <= 0) return;
-
-        if(weight - (cv * k) >= 0) {
-            dp[i][weight] = Math.max(dp[i][weight], dp[i-1][weight - (cv * k)] + (cc * k));
-        }
+        System.out.print(dp[M]);
     }
 }
