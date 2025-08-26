f002fbe8044772f06878871146a19313382775a9
GrowMyself <deae670@daum.net>
2025-08-26 22:37:20 +0900

    baekjoon: 랜덤마라톤 2주차 6번 풀이

---
+++ src/main/java/com/exercise/ps/Baekjoon/marathon/MR16435.java
@@ -0,0 +1,30 @@
+package com.exercise.ps.Baekjoon.marathon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MR16435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int len = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int j : arr) {
+            if (j > len) break;
+            len++;
+        }
+
+        System.out.print(len);
+    }
+}
